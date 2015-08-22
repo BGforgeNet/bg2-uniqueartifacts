0874b0e70a62ef841dbacb3c8baa232cb865c931
Ilya Ivanov <forn@ngs.ru>
2015-08-22 20:49:01 +0700

    add compatibility with almateria's restored items

--- uniqueartifacts/core/custom1.d
+++
@@ -1,3 +0,0 @@
-REPLACE_ACTION_TEXT ~nadari~
-  ~GiveItem("boot10",LastTalkedToBy)~
-  ~GiveItem("misc8j",LastTalkedToBy)~
--- uniqueartifacts/core/custom2.d
+++
@@ -1,3 +0,0 @@
-REPLACE_ACTION_TEXT ~ffbart~
-  ~GiveItemCreate("boot06",Player1,0,0,0)~
-  ~GiveItemCreate("boot10",Player1,0,0,0)~
